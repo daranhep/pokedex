d4082682b2d599e04bd10a4e22d013bf42357e1b
Dara Nhep <dara.h.nhep@gmail.com>
2015-12-12 16:48:59 -0600

    Initial Commit

---
+++ pokedex-by-dara/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-dara
+//
+//  Created by Dara Nhep on 12/12/15.
+//  Copyright © 2015 Dara Nhep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
